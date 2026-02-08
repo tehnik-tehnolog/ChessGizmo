567f58f572efa8e10e463c6c01d9db212390de12
Paul <pavelkos91011@gmail.com>
2026-02-09 00:01:19 +0300

    Change dtypes.py for PostgreSQL

--- dtypes.py
+++ dtypes.py
@@ -1,116 +1,70 @@
-from sqlalchemy import Integer, Boolean, SmallInteger, Float, VARCHAR, BIGINT
-from sqlalchemy.dialects.mysql import TINYINT
+from sqlalchemy.dialects.postgresql import TEXT, BIGINT, SMALLINT, BOOLEAN, REAL
 
-chess_df_users_dtypes = {
-    'username': VARCHAR(50),
-    'num_games': Integer
+# Data types for the users_df table
+users_dtypes = {
+    'username': TEXT,
+    'num_games': BIGINT
 }
 
-chess_games_info_dtypes = {
-    'id_game': VARCHAR(100),
-    'status': VARCHAR(50),
-    'id_player': VARCHAR(50),
-    'id_opponent': VARCHAR(50),
-    'main_color': Boolean,
-    'outcome': Float,
-    'main_rating': SmallInteger,
-    'enemy_rating': SmallInteger,
-    'opening_ACP': Float,
-    'mittelspiel_and_endgame_ACP': Float,
-    'is_there_endgame': Boolean,
-    'opening_STDPL': Float,
-    'mittelspiel_and_endgame_STDPL': Float,
-    'opening_ACP_by_cauchy': Float,
-    'mittelspiel_and_endgame_ACP_by_cauchy': Float,
-    'opening_STDPL_by_cauchy': Float,
-    'mittelspiel_and_endgame_STDPL_by_cauchy': Float
+# Data types for the games_info table
+games_info_dtypes = {
+    'id_game': TEXT,
+    'status': TEXT,
+    'id_player': TEXT,
+    'id_opponent': TEXT,
+    'main_color': BOOLEAN,
+    'main_rating': SMALLINT,
+    'enemy_rating': SMALLINT,
+    'winner': TEXT,
+    'opening_ACP': REAL,
+    'mittelspiel_and_endgame_ACP': REAL,
+    'is_there_endgame': BOOLEAN,
+    'opening_STDPL': REAL,
+    'mittelspiel_and_endgame_STDPL': REAL,
+    'opening_ACP_by_cauchy': REAL,
+    'mittelspiel_and_endgame_ACP_by_cauchy': REAL,
+    'opening_STDPL_by_cauchy': REAL,
+    'mittelspiel_and_endgame_STDPL_by_cauchy': REAL,
+    'av_opening_mobility_inc': REAL,
+    'av_mittelspiel_endgame_mobility_inc': REAL,
+    'av_opening_mobility_dec': REAL,
+    'av_mittelspiel_endgame_mobility_dec': REAL,
+    'av_opening_king_safety': TEXT,
+    'av_mittelspiel_endgame_king_safety': TEXT,
+    'av_opening_king_openness': REAL,
+    'av_mittelspiel_endgame_king_openness': REAL,
+    'av_opening_knight_activ_coeff': REAL,
+    'av_mittelspiel_endgame_knight_activ_coeff': REAL,
+    'av_opening_bishop_activ_coeff': REAL,
+    'av_mittelspiel_endgame_bishop_activ_coeff': REAL,
+    'av_opening_rook_queen_activ_coeff': REAL,
+    'av_mittelspiel_endgame_rook_queen_activ_coeff': REAL,
+    'av_mittelspiel_control': REAL,
+    'av_endgame_control': REAL,
+    'av_opening_control': REAL,
+    'outcome': REAL
 }
 
+# Data types for the games_by_moves table
 games_by_moves_dtypes = {
-    'id_game': VARCHAR(100),
-    'move_number': VARCHAR(6),
-    'white_move': VARCHAR(8),
-    'black_move': VARCHAR(8),
-    'white_move_index': TINYINT,
-    'black_move_index': TINYINT,
-    'analysis': Float,
-    'CP_loss': Float,
-    'CP_loss_by_cauchy': Float,
-    'pieces_material': SmallInteger,
-    'pawns': TINYINT,
-    'game_phase': VARCHAR(20),
-    'mobility_increment': Float,
-    'mobility_decrement': Float,
-    'control': TINYINT,
-    'king_safety': SmallInteger,
-    'king_openness': TINYINT,
-    'knight_activity_coeff': TINYINT,
-    'bishop_activity_coeff': TINYINT,
-    'rook_and_queen_activity_coeff': TINYINT,
+    'id_game': TEXT,
+    'move_number': SMALLINT,
+    'white_move': TEXT,
+    'black_move': TEXT,
+    'analysis': SMALLINT,
+    'CP_loss': SMALLINT,
+    'CP_loss_by_cauchy': REAL,
+    'pieces_material': SMALLINT,
+    'pawns': SMALLINT,
+    'game_phase': TEXT,
+    'mobility_increment': REAL,
+    'mobility_decrement': REAL,
+    'control': SMALLINT,
+    'king_safety': SMALLINT,
+    'king_openness': SMALLINT,
+    'knight_activity_coeff': SMALLINT,
+    'bishop_activity_coeff': SMALLINT,
+    'rook_and_queen_activity_coeff': SMALLINT,
+    'main_color': BOOLEAN
 }
 
-
-chess_df_users_dtypes_mod = {
-    'username': VARCHAR(50),
-    'num_games': Integer,
-    'type_player': VARCHAR(50)
-}
-
-games_by_moves_dtypes_mod = {
-    'id_game': VARCHAR(100),
-    'move_number': BIGINT,
-    'white_move': VARCHAR(6),
-    'black_move': VARCHAR(6),
-    'analysis': Float,
-    'CP_loss': Float,
-    'CP_loss_by_cauchy': Float,
-    'pieces_material': SmallInteger,
-    'pawns': TINYINT,
-    'game_phase': VARCHAR(50),
-    'mobility_increment': Float,
-    'mobility_decrement': Float,
-    'control': TINYINT,
-    'king_safety': SmallInteger,
-    'king_openness': TINYINT,
-    'knight_activity_coeff': TINYINT,
-    'bishop_activity_coeff': TINYINT,
-    'rook_and_queen_activity_coeff': TINYINT,
-    'main_color': VARCHAR(20)
-}
-
-chess_games_info_dtypes_mod = {
-    'id_game': VARCHAR(100),
-    'status': VARCHAR(50),
-    'id_player': VARCHAR(50),
-    'id_opponent': VARCHAR(50),
-    'main_color': VARCHAR(50), # Boolean,
-    'main_rating': SmallInteger,
-    'enemy_rating': SmallInteger,
-    'mittelspiel_and_endgame_ACP': Float,
-    'is_there_endgame': Boolean,
-    'opening_ACP': Float,
-    'opening_STDPL': Float,
-    'mittelspiel_and_endgame_STDPL': Float,
-    'opening_ACP_by_cauchy': Float,
-    'mittelspiel_and_endgame_ACP_by_cauchy': Float,
-    'opening_STDPL_by_cauchy': Float,
-    'mittelspiel_and_endgame_STDPL_by_cauchy': Float,
-    'av_opening_mobility_inc': Float,
-    'av_mittelspiel_endgame_mobility_inc': Float,
-    'av_opening_mobility_dec': Float,
-    'av_mittelspiel_endgame_mobility_dec': Float,
-    'av_opening_king_safety': Float,
-    'av_mittelspiel_endgame_king_safety': Float,
-    'av_opening_king_openness': Float,
-    'av_mittelspiel_endgame_king_openness': Float,
-    'av_opening_knight_activ_coeff': Float,
-    'av_mittelspiel_endgame_knight_activ_coeff': Float,
-    'av_opening_bishop_activ_coeff': Float,
-    'av_mittelspiel_endgame_bishop_activ_coeff': Float,
-    'av_opening_rook_queen_activ_coeff': Float,
-    'av_mittelspiel_endgame_rook_queen_activ_coeff': Float,
-    'av_mittelspiel_control': Float,
-    'av_endgame_control': Float,
-    'av_opening_control': Float,
-    'outcome': Float
-}
